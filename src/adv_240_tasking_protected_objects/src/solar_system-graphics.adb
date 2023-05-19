fabb9872a361393d115535d960a8ef51115d3af3
Léo Germond <germond@adacore.com>
2023-05-19 11:14:32 +0200

    update GPL-3.0

--- src/adv_240_tasking_protected_objects/src/solar_system-graphics.adb
+++ src/adv_240_tasking_protected_objects/src/solar_system-graphics.adb
@@ -20,3 +20,23 @@ package body Solar_System.Graphics is
    end Draw_All;
 
 end Solar_System.Graphics;
+-----------------------------------------------------------------------
+--                              Ada Labs                             --
+--                                                                   --
+--                 Copyright (C) 2008-2023, AdaCore                  --
+--                                                                   --
+-- This program is free software: you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public License as    --
+-- published by the Free Software Foundation, either version 3 of    --
+-- the License, or (at your option) any later version.               --
+--                                                                   --
+-- This program is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     --
+-- GNU General Public License for more details.                      --
+--                                                                   --
+-- You should have received a copy of the GNU General Public License --
+-- along with this program.  If not, see                             --
+-- <https://www.gnu.org/licenses/>.                                  --
+-----------------------------------------------------------------------
+
