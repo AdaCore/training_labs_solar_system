b4dbf21414b72d0e04a2ac4128df41921ce5e17a
Leo Germond <germond@adacore.com>
2024-10-16 14:33:28 +0200

    update copyright

--- src/160_genericity/answers/solar_system.ads
+++ src/160_genericity/answers/solar_system.ads
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------------
 --                              Ada Labs                             --
 --                                                                   --
---                 Copyright (C) 2008-2023, AdaCore                  --
+--                 Copyright (C) 2008-2024, AdaCore                  --
 --                                                                   --
 -- Labs is free  software; you can redistribute it and/or modify  it --
 -- under the terms of the GNU General Public License as published by --
